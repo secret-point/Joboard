5140ec5aec8df3704b49dbda6ff102cf252b9335
secretpoint <secret.point2020@gmail.com>
2020-07-20 17:05:00 -0700

    Increasing hearbeat event for 60 minutes.
    cr https://code.amazon.com/reviews/CR-29985389

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,4 @@ export const IN_PROGRESS = "IN_PROGRESS";
 export const PENDING = "PENDING";
 export const COMPLETED = "COMPLETED";
 
-export const MAX_MINUTES_FOR_HEARTBEAT = 30;
+export const MAX_MINUTES_FOR_HEARTBEAT = 60;
