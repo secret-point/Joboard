6aff6cb26e734064343e796048930d57d7ccd80e
secretpoint <secret.point2020@gmail.com>
2022-08-15 20:01:31 +0000

    remove link format on shift selection page
    cr: https://code.amazon.com/reviews/CR-74310076

--- src/components/common/jobOpportunity/JobConfirmationCard.tsx
+++ src/components/common/jobOpportunity/JobConfirmationCard.tsx
@@ -68,8 +68,11 @@ const JobConfirmationCard = ( props: JobConfirmationCardProps ) => {
             </Row>
             {
                 scheduleBannerText &&
-                <Row padding={{ left: 'S300', right: 'S300', bottom: 'S300' }}>
-                    <Link fontSize='T100'>{scheduleBannerText}</Link>
+                <Row
+                  padding={{ left: 'S300', right: 'S300', bottom: 'S300' }}
+                  className="scheduleBannerText"
+                >
+                    <Text fontSize='T100'>{scheduleBannerText}</Text>
                 </Row>
             }
             <Row
