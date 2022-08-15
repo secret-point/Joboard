f4a927d34ec04215026d878751d82bb484df4f93
secretpoint <secret.point2020@gmail.com>
2022-08-15 16:46:35 -0700

    Fix: loading text should be translated
    SIM: https://i.amazon.com/MLS_Defect-216
    cr: https://code.amazon.com/reviews/CR-74327714

--- src/components/common/AppLoader.tsx
+++ src/components/common/AppLoader.tsx
@@ -3,6 +3,7 @@ import { Col } from "@amzn/stencil-react-components/layout";
 import { Spinner, SpinnerSize } from "@amzn/stencil-react-components/spinner";
 import { connect } from "react-redux";
 import { uiState } from "../../reducers/ui.reducer";
+import { translate as t } from "../../utils/translator";
 
 interface MapStateToProps {
     ui: uiState
@@ -23,7 +24,7 @@ export const AppLoader = ( props: AppLoaderMergedProps) => {
         <Col
             className="AppLoader"
         >
-            <Spinner size={SpinnerSize.Large} loadingText={loaderText}/>
+            <Spinner size={SpinnerSize.Large} loadingText={loaderText || t("BB-spinner-loading-text", 'Loading...') }/>
         </Col> :
             <></>
     )
