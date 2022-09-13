42726d57d32874c8bf4130d6c2459be52f44118c
secretpoint <secret.point2020@gmail.com>
2022-09-13 14:43:07 -0700

    Do not redirect to wotc page on wotc-complete page
    SIM: https://i.amazon.com/MLS_Defect-389
    cr: https://code.amazon.com/reviews/CR-75931724

--- src/components/us/wotc/WotcComplete.tsx
+++ src/components/us/wotc/WotcComplete.tsx
@@ -1,33 +1,34 @@
 import React, { useEffect } from "react";
-import queryString from "query-string";
 import { Col } from "@amzn/stencil-react-components/layout";
+import queryString from "query-string";
+import { connect } from "react-redux";
 import { useLocation } from "react-router";
+import { addMetricForPageLoad } from "../../../actions/AdobeActions/adobeActions";
+import { boundGetApplication } from "../../../actions/ApplicationActions/boundApplicationActions";
+import { boundGetCandidateInfo } from "../../../actions/CandidateActions/boundCandidateActions";
+import { boundGetJobDetail } from "../../../actions/JobActions/boundJobDetailActions";
+import { boundGetScheduleDetail } from "../../../actions/ScheduleActions/boundScheduleActions";
+import { onCompleteTaskHelper } from "../../../actions/WorkflowActions/workflowActions";
+import { boundUpdateWotcStatus } from "../../../actions/WotcActions/boundWotcAction";
 import {
   getPageNameFromPath,
   parseQueryParamsArrayToSingleItem,
   resetIsPageMetricsUpdated
 } from "../../../helpers/utils";
 import { ApplicationState } from "../../../reducers/application.reducer";
-import { boundGetApplication } from "../../../actions/ApplicationActions/boundApplicationActions";
-import { boundUpdateWotcStatus } from "../../../actions/WotcActions/boundWotcAction";
-import { checkAndBoundGetApplication, getLocale } from "../../../utils/helper";
-import { connect } from "react-redux";
-import { addMetricForPageLoad } from "../../../actions/AdobeActions/adobeActions";
-import { Application, UpdateWotcStatusRequest } from "../../../utils/types/common";
 import { CandidateState } from "../../../reducers/candidate.reducer";
-import { boundGetCandidateInfo } from "../../../actions/CandidateActions/boundCandidateActions";
-import { onCompleteTaskHelper } from "../../../actions/WorkflowActions/workflowActions";
-import { boundGetScheduleDetail } from "../../../actions/ScheduleActions/boundScheduleActions";
-import { boundGetJobDetail } from "../../../actions/JobActions/boundJobDetailActions";
-import { ScheduleState } from "../../../reducers/schedule.reducer";
 import { JobState } from "../../../reducers/job.reducer";
+import { ScheduleState } from "../../../reducers/schedule.reducer";
+import { WORKFLOW_STEP_NAME } from "../../../utils/enums/common";
+import { checkAndBoundGetApplication, getLocale } from "../../../utils/helper";
+import { Application, UpdateWotcStatusRequest } from "../../../utils/types/common";
 
 interface MapStateToProps {
   candidate: CandidateState;
   application: ApplicationState;
   schedule: ScheduleState;
-  job: JobState,
-};
+  job: JobState;
+}
 
 const WOTC_COMPLETE_STATUS = "Completed";
 
@@ -38,7 +39,7 @@ export const WotcComplete = (props: MapStateToProps) => {
   const queryParams = parseQueryParamsArrayToSingleItem(queryString.parse(search));
   const { applicationId, jobId, scheduleId } = queryParams;
   const applicationData = application.results;
-  const scheduleDetail = schedule.results.scheduleDetail;
+  const { scheduleDetail } = schedule.results;
   const { candidateData } = candidate.results;
   const jobDetail = job.results;
 
@@ -85,6 +86,11 @@ export const WotcComplete = (props: MapStateToProps) => {
       boundUpdateWotcStatus(request, (data: any) => {
         // get the latest application data and update the application state, then complete the step
         boundGetApplication({ applicationId: applicationId, locale: getLocale() }, (app: Application) => {
+          // On wotc complete, we're not completing wotc-complete step (no step available in backend).
+          // Instead, we should complete the wotc step.
+          // onCompleteTaskHelper(app, false, undefined, WORKFLOW_STEP_NAME.WOTC);
+
+          // https://issues.amazon.com/issues/MLS_Defect-389, keep to use wotc-complete step for now
           onCompleteTaskHelper(app);
         });
       });
