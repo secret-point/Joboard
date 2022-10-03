7b4431532b7e10905e034084fe752630be5fbc75
secretpoint <secret.point2020@gmail.com>
2022-10-03 09:34:02 -0700

    Fix: moment should not have moment format
    cr: https://code.amazon.com/reviews/CR-77066573

--- src/components/common/jobOpportunity/ScheduleDetails.tsx
+++ src/components/common/jobOpportunity/ScheduleDetails.tsx
@@ -41,7 +41,7 @@ const ScheduleDetails = (props: ScheduleDetailsProps) => {
     totalPayRateL10N
   } = scheduleDetail;
 
-  const startDate = firstDayOnSiteL10N ? `${moment(firstDayOnSiteL10N,"MM DD, YYYY" ).locale(getLocale()).format('MM DD, YYYY')}`: firstDayOnSite;
+  const startDate = firstDayOnSiteL10N ? `${moment(firstDayOnSiteL10N).locale(getLocale()).format('MMM DD, YYYY')}`: firstDayOnSite;
 
   //TODO revert the function back once translations are in
   const renderRequiredLanguages = (schedule: Schedule): string => {
