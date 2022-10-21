0c0ba2c9d211ed82e5c704831ecf7933d91c59dd
secretpoint <secret.point2020@gmail.com>
2022-10-21 17:16:27 +0000

    Increased coverage for ReviewSubmit component
    # This is a combination of 2 commits.
    
    commit: 6d3ce30053f246916bea399e257eaef7a0955f81
    Author: Mustafa [C] Ay <mustaay@amazon.com>
    Date: 2022-10-21T16:06:19.000Z
    
    CR fixes
    
    commit: d6a73598769fe6303d3c3ea77ffb6f0f2e8467a2
    Author: Mustafa [C] Ay <mustaay@amazon.com>
    Date: 2022-10-20T21:45:33.000Z
    
    Increased coverage for ReviewSubmit component

--- tests/unit-tests/components/us/reviewSubmit/ReviewSubmitTest.tsx
+++ tests/unit-tests/components/us/reviewSubmit/ReviewSubmitTest.tsx
@@ -3,6 +3,9 @@ import { shallow } from "enzyme";
 import { useLocation } from 'react-router-dom';
 import { ReviewSubmit } from "../../../../../src/components/us/reviewSubmit/ReviewSubmit";
 import { TEST_APPLICATION_ID, TEST_APPLICATION_STATE, TEST_CANDIDATE_STATE, TEST_JOB_ID, TEST_JOB_STATE, TEST_SCHEDULE_ID, TEST_SCHEDULE_STATE } from "../../../../test-utils/test-data";
+import { act } from "react-dom/test-utils";
+import * as boundUi from "../../../../../src/actions/UiActions/boundUi";
+import * as workflowActions from "../../../../../src/actions/WorkflowActions/workflowActions";
 
 describe("ReviewSubmit", () => {
   const mockLocation = {
@@ -25,4 +28,45 @@ describe("ReviewSubmit", () => {
 
     expect(shallowWrapper).toMatchSnapshot();
   });
+
+  it("should call boundResetBannerMessage", () => {
+    const shallowWrapper = shallow(
+      <ReviewSubmit
+        candidate={TEST_CANDIDATE_STATE}
+        job={TEST_JOB_STATE}
+        application={TEST_APPLICATION_STATE}
+        schedule={TEST_SCHEDULE_STATE}
+      />);
+
+    const spy = jest.spyOn(boundUi, 'boundResetBannerMessage');
+
+    const submitButton = shallowWrapper.find('DebouncedButton').first();
+
+    act(() => {
+      submitButton.simulate('click');
+    })
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it("should call boundResetBannerMessage", () => {
+    const shallowWrapper = shallow(
+      <ReviewSubmit
+        candidate={TEST_CANDIDATE_STATE}
+        job={TEST_JOB_STATE}
+        application={TEST_APPLICATION_STATE}
+        schedule={TEST_SCHEDULE_STATE}
+      />);
+
+    const spy = jest.spyOn(workflowActions, 'onCompleteTaskHelper');
+
+    const editButtons = shallowWrapper.find('Button');
+
+    act(() => {
+      editButtons.forEach(button => button.simulate('click'));
+    })
+
+    expect(spy).toHaveBeenCalledTimes(editButtons.length);
+  });
 });
+
