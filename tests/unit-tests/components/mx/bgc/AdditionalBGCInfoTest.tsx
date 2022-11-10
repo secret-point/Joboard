0a406cd543be8f8b9c7df515214807a200f943a7
secretpoint <secret.point2020@gmail.com>
2022-11-10 00:50:47 +0000

    Unit tests for `mx/bgc`

---
+++ tests/unit-tests/components/mx/bgc/AdditionalBGCInfoTest.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { AdditionalBGCInfo } from "../../../../../src/components/mx/bgc/AdditionalBGCInfo";
+import { TEST_APPLICATION_STATE, TEST_BGC_STATE, TEST_CANDIDATE_STATE, TEST_JOB_STATE, TEST_SCHEDULE_STATE } from "../../../../test-utils/test-data";
+
+describe("AdditionalBGCInfo", () => {
+  
+  it("should match snapshot", () => {
+    const shallowWrapper = shallow(
+      <AdditionalBGCInfo
+        appConfig={{}}
+        candidate={TEST_CANDIDATE_STATE}
+        job={TEST_JOB_STATE}
+        application={TEST_APPLICATION_STATE}
+        schedule={TEST_SCHEDULE_STATE}
+        bgc={TEST_BGC_STATE}
+      />);
+
+    expect(shallowWrapper).toMatchSnapshot();
+  });
+});
