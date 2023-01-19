a41cb45cdb23ab40b81a10b78a69b94faab34253
secretpoint <secret.point2020@gmail.com>
2023-01-19 15:28:22 +0000

    Extending the StepHeader component to accept the withAssessment prop to inform the amount of steps displaying in the flyout

--- src/components/common/StepHeader.tsx
+++ src/components/common/StepHeader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, ButtonVariant } from "@amzn/stencil-react-components/button";
 import { FlyoutContent, RenderFlyoutFunctionParams, WithFlyout } from "@amzn/stencil-react-components/flyout";
-import { Col } from "@amzn/stencil-react-components/layout";
+import { Col, Flex } from "@amzn/stencil-react-components/layout";
 import { Text } from "@amzn/stencil-react-components/text";
 import { CommonColors } from "../../utils/colors";
 import { translate as t } from "../../utils/translator";
@@ -11,10 +11,12 @@ import ApplicationSteps from "./ApplicationSteps";
 interface StepHeaderProps {
   jobTitle: string;
   step: ApplicationStep;
+  withAssessment?: boolean,
+
 }
 export const StepHeader = (props: StepHeaderProps) => {
 
-  const { jobTitle, step } = props;
+  const { jobTitle, step, withAssessment = false } = props;
 
   const renderFlyout = ({ close }: RenderFlyoutFunctionParams) => (
     <FlyoutContent
@@ -26,29 +28,32 @@ export const StepHeader = (props: StepHeaderProps) => {
         </Button>
       ]}
     >
-      <ApplicationSteps />
+      <ApplicationSteps withAssessment={withAssessment} />
     </FlyoutContent>
   );
 
   return (
     <Col id="stepHeaderContainer" gridGap={5} padding="S300" style={{ background: `${CommonColors.Blue05}` }}>
       <Text>{jobTitle}</Text>
-      <WithFlyout renderFlyout={renderFlyout}>
-        {( { open } ) => (
-          <Col alignItems="flex-end">
-            <Text
-              fontSize="T100"
-              color={CommonColors.Blue70}
-              style={{ cursor: "pointer" }}
-              onClick={() => open()}
-              fontWeight="medium"
-            >
-              {t("BB-StepHeader-view-progress-button", "View progress")}
-            </Text>
-          </Col>
-        )}
-      </WithFlyout>
-      <Text fontSize="T100">{`${step.stepNumber}. ${t(step.titleTranslationKey, step.title)}`}</Text>
+
+      <Flex flexDirection="row" gridGap="S300" justifyContent="space-between">
+        <Text fontSize="T100">{`${step.stepNumber}. ${t(step.titleTranslationKey, step.title)}`}</Text>
+        <WithFlyout renderFlyout={renderFlyout}>
+          {({ open }) => (
+            <Col alignItems="flex-end">
+              <Text
+                fontSize="T100"
+                color={CommonColors.Blue70}
+                style={{ cursor: "pointer" }}
+                onClick={() => open()}
+                fontWeight="medium"
+              >
+                {t("BB-StepHeader-view-progress-button", "View progress")}
+              </Text>
+            </Col>
+          )}
+        </WithFlyout>
+      </Flex>
     </Col>
   );
 };
