86555e44790c3ef1570df24c1cee17cd1adce63d
secretpoint <secret.point2020@gmail.com>
2023-03-01 16:25:33 +0200

    Merging job-helpers into schedule helpers as it is more relevant. Also enabling the translation for the duration strings

--- src/helpers/schedule-helper.ts
+++ src/helpers/schedule-helper.ts
@@ -1,6 +1,48 @@
 import { Schedule } from "../utils/types/common";
-import { getScheduleDuration } from "./job-helpers";
 import { getLocalizedDate, get12hrTimeStringLocalized } from "./localization-helpers";
+import { log } from "./log-helper";
+import { translate as t } from "../utils/translator";
+
+export const PERMANENT_CONTRACT_STRING = t(
+  "BB-schedule-card-duration-permanent", "Permanent"
+);
+export const FIXED_TERM_CONTRACT_STRING = t(
+  "BB-schedule-card-duration-fixed-term-contract", "Fixed term contract"
+);
+export const FIXED_TERM_CONTRACT_WITH_END_DATE_STRING = t(
+  "BB-schedule-card-duration-fixed-term-contract-with-end-date", "Fixed term contract until"
+);
+
+interface iScheduleDurationFn {
+  employmentType: string;
+  hireEndDate: string | null;
+  scheduleId: string;
+  applicationId?: string;
+}
+
+export const getScheduleDuration = ({
+  employmentType,
+  hireEndDate,
+  scheduleId,
+  applicationId
+}: iScheduleDurationFn) => {
+  let duration: null | string = null;
+
+  if (employmentType === "Regular") {
+    duration = !hireEndDate
+      ? PERMANENT_CONTRACT_STRING
+      : `${FIXED_TERM_CONTRACT_WITH_END_DATE_STRING} ${hireEndDate}`;
+  } else if (employmentType === "Seasonal") {
+    duration = hireEndDate ? null : FIXED_TERM_CONTRACT_STRING;
+  }
+  log(
+    `The duration for schedule ${scheduleId}  ${
+      applicationId ? `and applicationId ${applicationId}` : ""
+    } is set to ${duration}`
+  );
+
+  return duration;
+};
 
 export const getScheduleInUKFormat = (schedule: Schedule) => {
 
--- tests/unit-tests/helpers/schedule-helper.test.ts
+++ tests/unit-tests/helpers/schedule-helper.test.ts
@@ -1,30 +1,69 @@
-import { getScheduleInUKFormat } from "../../../src/helpers/schedule-helper";
+import { getScheduleInUKFormat, FIXED_TERM_CONTRACT_STRING, FIXED_TERM_CONTRACT_WITH_END_DATE_STRING, getScheduleDuration, PERMANENT_CONTRACT_STRING } from "../../../src/helpers/schedule-helper";
 import { HVH_LOCALE } from "../../../src/utils/constants/common";
 import { Locale } from "../../../src/utils/types/common";
 import { TEST_SCHEDULE } from "../../test-utils/test-data";
 
+const sheduleWithEndDateRegular = {
+  employmentType: "Regular",
+  hireEndDate: "2022-12-31",
+  scheduleId: "JOB-123"
+};
+const sheduleWithoutEndDateRegular = {
+  ...sheduleWithEndDateRegular,
+  hireEndDate: null
+};
+const sheduleWithEndDateSeasonal = {
+  employmentType: "Seasonal",
+  hireEndDate: "2022-12-31",
+  scheduleId: "JOB-123"
+};
+const sheduleWithoutEndDateSeasonal = {
+  ...sheduleWithEndDateSeasonal,
+  hireEndDate: null
+};
+
 describe("schedule-helpers", () => {
+
+  describe("getScheduleDuration", () => {
+    test("Calculating the duration when Employment type is regular plus different variations of schedule  ", () => {
+      const withEndDate = getScheduleDuration(sheduleWithEndDateRegular);
+      expect(withEndDate).toContain(FIXED_TERM_CONTRACT_WITH_END_DATE_STRING);
   
-  const setLocaleToCookies = (locale: string) => Object.defineProperty(window.document, "cookie", {
-    writable: true,
-    value: `${HVH_LOCALE}=${locale}`,
-  }); 
+      const withOutEndDate = getScheduleDuration(sheduleWithoutEndDateRegular);
+      expect(withOutEndDate).toBe(PERMANENT_CONTRACT_STRING);
+    });
+    test("Calculating the duration when Employment type is Seasonal plus different variations of schedule  ", () => {
+      const withEndDate = getScheduleDuration(sheduleWithEndDateSeasonal);
+      expect(withEndDate).toBe(null);
+  
+      const withOutEndDate = getScheduleDuration(sheduleWithoutEndDateSeasonal);
+      expect(withOutEndDate).toBe(FIXED_TERM_CONTRACT_STRING);
+    });
+  });
 
-  test("getScheduleInUKFormat", () => {
+  describe("getScheduleInUKFormat", () => {
+  
+    const setLocaleToCookies = (locale: string) => Object.defineProperty(window.document, "cookie", {
+      writable: true,
+      value: `${HVH_LOCALE}=${locale}`,
+    }); 
 
-    const scheduleData = {
-      ...TEST_SCHEDULE, 
-      hireEndDate: "2022-10-12"
-    };
-    setLocaleToCookies(Locale.enGB);
-    const schedule = getScheduleInUKFormat(scheduleData);
+    test("getScheduleInUKFormat", () => {
 
-    expect(schedule.duration).toBeDefined();
-    expect(schedule.hireEndDate).toBe("Wednesday, 12 Oct 2022");
+      const scheduleData = {
+        ...TEST_SCHEDULE, 
+        hireEndDate: "2022-10-12"
+      };
+      setLocaleToCookies(Locale.enGB);
+      const schedule = getScheduleInUKFormat(scheduleData);
 
-    expect(schedule.displayHireEndDate).toBeFalsy();
-    expect(schedule.displayEmploymentType).toBeDefined();
+      expect(schedule.duration).toBeDefined();
+      expect(schedule.hireEndDate).toBe("Wednesday, 12 Oct 2022");
 
+      expect(schedule.displayHireEndDate).toBeFalsy();
+      expect(schedule.displayEmploymentType).toBeDefined();
+
+    }); 
   });
 
 });
